72a01e5006860b4a7243f0a0b0ef9d2e75d2759a
Jun Alvior <junalvior21@gmail.com>
2025-10-24 17:41:02 +0800

    fix some shits

--- reservation_system/reservations/urls.py
+++ reservation_system/reservations/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.reservation_list, name='reservation_list'),
     path('new/', views.create_reservation, name='create_reservation'),
-    path('/edit/', views.edit_reservation, name='edit_reservation'),
-    path('/delete/', views.delete_reservation, name='delete_reservation'),
+    path('<int:id>/edit/', views.edit_reservation, name='edit_reservation'),  # Fixed!
+    path('<int:id>/delete/', views.delete_reservation, name='delete_reservation'),  # Fixed!
 ]
