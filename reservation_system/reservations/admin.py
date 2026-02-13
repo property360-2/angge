b0da9e90724f7fe72ee32e76d05d6ef3b01c6ff3
Jun Alvior <junalvior21@gmail.com>
2026-02-13 16:33:46 +0800

    feat: Implement reservation management UI (list, create, edit, delete) and configure Django admin for reservations.

--- reservation_system/reservations/admin.py
+++ reservation_system/reservations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'date', 'time', 'guests', 'created_at']
+    list_filter = ['date', 'created_at']
+    search_fields = ['name', 'user__username']
+    date_hierarchy = 'date'
